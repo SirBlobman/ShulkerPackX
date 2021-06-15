3ed53bed4c3bee2ddaae35cb3543b9dae31c8cfe
SirBlobman <blobmansrealm@gmail.com>
2021-06-14 23:20:32 -0400

    1.3.1: Added update checker.

--- src/main/java/com/github/sirblobman/shulker/ShulkerPlugin.java
+++ src/main/java/com/github/sirblobman/shulker/ShulkerPlugin.java
@@ -18,6 +18,8 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.sirblobman.api.core.CorePlugin;
+import com.github.sirblobman.api.update.UpdateManager;
 import com.github.sirblobman.shulker.menu.ShulkerBoxMenu;
 
 public class ShulkerPlugin extends JavaPlugin implements Listener {
@@ -25,6 +27,10 @@ public class ShulkerPlugin extends JavaPlugin implements Listener {
     public void onEnable() {
         PluginManager manager = Bukkit.getPluginManager();
         manager.registerEvents(this, this);
+
+        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
+        UpdateManager updateManager = corePlugin.getUpdateManager();
+        updateManager.addResource(this, 81793L);
     }
     
     @EventHandler(priority=EventPriority.NORMAL)
