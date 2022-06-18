bdb66c565513fed3c850246b2cb71fa87bb8f846
SirBlobman <blobmansrealm@gmail.com>
2022-06-18 09:39:13 -0400

    Minor code style changes.

--- src/main/java/com/github/sirblobman/shulker/ShulkerPlugin.java
+++ src/main/java/com/github/sirblobman/shulker/ShulkerPlugin.java
@@ -1,13 +1,18 @@
 package com.github.sirblobman.shulker;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.sirblobman.api.core.CorePlugin;
+import com.github.sirblobman.api.plugin.ConfigurablePlugin;
 import com.github.sirblobman.api.update.UpdateManager;
 import com.github.sirblobman.shulker.listener.ListenerMenu;
 
-public final class ShulkerPlugin extends JavaPlugin implements Listener {
+public final class ShulkerPlugin extends ConfigurablePlugin {
+    @Override
+    public void onLoad() {
+        // Do Nothing
+    }
+
     @Override
     public void onEnable() {
         new ListenerMenu(this).register();
@@ -15,5 +20,10 @@ public final class ShulkerPlugin extends JavaPlugin implements Listener {
         CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
         UpdateManager updateManager = corePlugin.getUpdateManager();
         updateManager.addResource(this, 81793L);
+    }
+
+    @Override
+    public void onDisable() {
+        // Do Nothing
     }
 }
