20436710928baf53c89da99ef5ce5bc529d60079
SirBlobman <blobmansrealm@gmail.com>
2022-08-18 09:03:59 -0400

    1.4.0: Update to BlueSlimeCore 2.5.2.

--- src/main/java/com/github/sirblobman/shulker/ShulkerPlugin.java
+++ src/main/java/com/github/sirblobman/shulker/ShulkerPlugin.java
@@ -2,7 +2,11 @@ package com.github.sirblobman.shulker;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.sirblobman.api.bstats.bukkit.Metrics;
+import com.github.sirblobman.api.bstats.charts.SimplePie;
 import com.github.sirblobman.api.core.CorePlugin;
+import com.github.sirblobman.api.language.Language;
+import com.github.sirblobman.api.language.LanguageManager;
 import com.github.sirblobman.api.plugin.ConfigurablePlugin;
 import com.github.sirblobman.api.update.UpdateManager;
 import com.github.sirblobman.shulker.listener.ListenerMenu;
@@ -15,15 +19,34 @@ public final class ShulkerPlugin extends ConfigurablePlugin {
 
     @Override
     public void onEnable() {
-        new ListenerMenu(this).register();
-
-        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
-        UpdateManager updateManager = corePlugin.getUpdateManager();
-        updateManager.addResource(this, 81793L);
+        registerListeners();
+        registerUpdateChecker();
+        registerbStats();
     }
 
     @Override
     public void onDisable() {
         // Do Nothing
+    }
+
+    private void registerListeners() {
+        new ListenerMenu(this).register();
+    }
+
+    private void registerUpdateChecker() {
+        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
+        UpdateManager updateManager = corePlugin.getUpdateManager();
+        updateManager.addResource(this, 81793L);
+    }
+
+    private void registerbStats() {
+        Metrics metrics = new Metrics(this, 16178);
+        metrics.addCustomChart(new SimplePie("selected_language", this::getDefaultLanguageCode));
+    }
+
+    private String getDefaultLanguageCode() {
+        LanguageManager languageManager = getLanguageManager();
+        Language defaultLanguage = languageManager.getDefaultLanguage();
+        return (defaultLanguage == null ? "none" : defaultLanguage.getLanguageCode());
     }
 }
